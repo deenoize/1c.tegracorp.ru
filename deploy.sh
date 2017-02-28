e92ca0b5ae49b5f6561d57ee7548657712e06fa1
Russel Lvov <deenoize@gmail.com>
2017-02-28 23:13:38 +0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+-#!/bin/bash		
+ -		
+ -ssh -v ec2-user@52.204.34.222 << EOF		
+ -		
+ -echo '1. Updating sources'		
+ -cd /var/www/tegracorp.ru/
+ -rm -rf .* *
+ -git clone https://github.com/deenoize/tegracorp.ru.git		
+ -		
+ -echo 'Done!'		
+ -		
+ -EOF
