3a7669c5c4612e6438aaf50bb6f94d8e8b398c5c
Russel Lvov <deenoize@gmail.com>
2017-03-01 17:32:30 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
--#!/bin/bash		
- -		
- -ssh -v ec2-user@52.204.34.222 << EOF		
- -		
- -echo '1. Updating sources'		
- -cd /var/www/tegracorp.ru/
- -rm -rf .* *
- -git clone https://github.com/deenoize/tegracorp.ru.git		
- -		
- -echo 'Done!'		
- -		
- -EOF
+#!/bin/bash		
+ ssh -v ec2-user@52.204.34.222 << EOF		
+ 
+ echo '1. Updating sources'		
+ cd /var/www/tegracorp.ru/
+ rm -rf .* *
+ git clone https://github.com/deenoize/tegracorp.ru.git		
+ 		
+ echo 'Done!'		
+ 		
+ EOF
